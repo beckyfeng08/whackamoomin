f3738c7ebcfdb65de401423468d70f4b2813e990
Rebecca Feng <beckyfeng08@berkeley.edu>
2023-12-16 11:55:46 -0800

    add timer intervals

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ function randomSquare() {
 
     let randomSquare = squares[Math.floor(Math.random()*9)];
     randomSquare.classList.add('mole');
-
+    console.log("changed")
     hitPosition = randomSquare.id;
 }
 
@@ -26,9 +26,11 @@ squares.forEach(square => {
             result++;
             score.textContent = result;
             hitPosition = null;
+
             setTimeout(function() {
                 square.classList.remove("clickedhit");
               }, 200);
+              
         } else {
             square.classList.toggle("clickedmiss");
             setTimeout(function() {
@@ -40,15 +42,29 @@ squares.forEach(square => {
 })
 
 function moveMole() {
+    if (currentTime > 50) {
+        timerId = setInterval(randomSquare, 1000);
+    } else if (currentTime > 40) {
+        timerId = setInterval(randomSquare, 900);
+    } else if (currentTime > 30) {
+        timerId = setInterval(randomSquare, 800);
+    } else if (currentTime > 20) {
+        timerId = setInterval(randomSquare, 700);
+    } else if (currentTime > 10) {
+        timerId = setInterval(randomSquare, 600);
+    } else {
+        timerId = setInterval(randomSquare, 500);
+    }
     
-    timerId = setInterval(randomSquare, 500);
+   
+    
 }
 
-moveMole();
 
 function countDown() {
     currentTime --;
     timeLeft.textContent = currentTime;
+    moveMole();
     if (currentTime == 0) {
         clearInterval(countDownTimerId);
         clearInterval(timerId);
